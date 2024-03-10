7847a493ebf8bcdb856daabe8410f7356a8d05f1
Abdullohtursunov <tursunovabdulloh172@gmail.com>
2024-03-10 03:55:14 +0300

    1

---
+++ app.js
@@ -0,0 +1,27 @@
+// let upvotes = +prompt("son kiriting");
+// let downvotes = +prompt("son kiriting");
+// function ovozlar(plus, mines) {
+//   let result = plus - mines;
+//   return result;
+// }
+// console.log(ovozlar(upvotes, downvotes));
+
+// let son = +prompt("son kiriting");
+// function negativ(son) {
+//   if (son) {
+//     let result = "-" + son;
+//     return result;
+//   }
+// }
+// console.log(negativ(son));
+
+// let user = {
+//   ism: "Ikrom",
+//   raqam: 993932929,
+//   city: "Margilan",
+// };
+// console.log(user);
+// let tushirishSoni = +prompt("son kiriting")
+// function tushirMassiv(arr, tushirishSoni) {
+
+// }
